fa45cee52764e33c6f5031d6656698eda955d309
ursha <benas.pekarskis@gmail.com>
2021-02-17 17:09:14 +0000

    Add files via upload

---
+++ Blog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    title= models.CharField(max_length=255)
+    title_tag= models.CharField(max_length=255,default="OmniGeo")
+    author=models.ForeignKey(User,on_delete=models.CASCADE)
+    body=models.TextField()
+
+
+
+    def __str__(self):
+        return self.title +'-'+str(self.author)
+    
